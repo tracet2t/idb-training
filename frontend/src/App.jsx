a0bda3a0e4e33ae9c121747c6470eae45155429c
Afzaan <mufnah@gmail.com>
2026-04-12 00:41:10 +0530

    work in progress - participants UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Participants from "./pages/Participants";
+import Analytics from "./pages/Analytics";
+import Programs from "./pages/Programs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/participants" element={<Participants />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/programs" element={<Programs />} />
       </Routes>
     </BrowserRouter>
   );
